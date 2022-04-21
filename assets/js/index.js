2201b07040adec6d48e3f0a57f9ac28905335a96
sk8terboyz <kennedy.shane@att.net>
2022-04-21 18:24:58 -0500

    get/post request template + button links + cart

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,27 @@
-sessionStorage.setItem('hello','world');
+$(document).ready(function() {
 
-//test comment trbern1
+    function getMenuItems() {
+        const menuItemUrl = ''
+        $.get(menuItemUrl, function(data, status) {
+            alert("Data: " + data + "\nStatus: " + status);
+        }).fail(function() {
+            console.error("Error while getting menu items")
+        })
+    }
+    
+    function createOrder() {
+        const createOrderUrl = ''
+        // test data
+        var order = {
+            order_id: sessionStorage.getItem('orderID'),
+            item_id: sessionStorage.getItem('itemID'),
+            quant: sessionStorage.getItem('quantity')
+        }
+        $.post(createOrderUrl, order, function(data, status) {
+            alert("Post")
+        }).fail(function() {
+            alert('error in post request')
+        })
+    }
+
+})
