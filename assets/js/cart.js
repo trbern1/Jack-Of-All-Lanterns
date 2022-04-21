638aafc53e4ff5369185130d9723113e9ce4c48c
sk8terboyz <kennedy.shane@att.net>
2022-04-21 18:36:44 -0500

    moved order to menu.js and added some cart logic

---
+++ assets/js/cart.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    function setQuantity() {
+        // retrieve quantity from cart
+        var quantity = 0;
+        // set quantity in session storage
+        sessionStorage.setItem('quantity', quantity)
+    }
+
+    function setOrderID() {
+        // retrieve quantity from cart
+        var order_id = 0;
+        // set quantity in session storage
+        sessionStorage.setItem('orderID', quantity)
+    }
+
+    function setItemID() {
+        // retrieve quantity from cart
+        var quantity = 0;
+        // set quantity in session storage
+        sessionStorage.setItem('itemID', quantity)
+    }
+
+})
