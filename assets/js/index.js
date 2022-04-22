2b80b0e05f217b94c05ee05ea80f4efac5b1d8e3
Tegan Bernard <trbern1@ilstu.edu>
2022-04-22 14:29:28 -0500

    Added basic about us layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,33 @@
-$(document).ready(function() {
+// $(document).ready(function() {
     
-    const fakeKeys = [0,1,2,3,4];
-    const MAX = 10
-    function createOrderID() {
-        var key = Math.floor(Math.random() * MAX);
-        while(!checkOrderID(key)) { key = Math.floor(Math.random() * MAX); }
-        return key;
-    }
+//     const fakeKeys = [0,1,2,3,4];
+//     const MAX = 10
+//     function createOrderID() {
+//         var key = Math.floor(Math.random() * MAX);
+//         while(!checkOrderID(key)) { key = Math.floor(Math.random() * MAX); }
+//         return key;
+//     }
     
-    function checkOrderID(key) {
-        var check = false;
-        if(Number.isInteger(key)) {
-            // compare the key to the already made set of keys
-            $.get(`/order/${key}`, function(data, status) {
-                console.error('key is already in the database');
-                check = false;
-            }).fail(function() {
-                // continue with process because key is unique
-                console.error("Error while getting menu items")
-                check = true;
-            })
+//     function checkOrderID(key) {
+//         var check = false;
+//         if(Number.isInteger(key)) {
+//             // compare the key to the already made set of keys
+//             $.get(`/order/${key}`, function(data, status) {
+//                 console.error('key is already in the database');
+//                 check = false;
+//             }).fail(function() {
+//                 // continue with process because key is unique
+//                 console.error("Error while getting menu items")
+//                 check = true;
+//             })
             
-            return check;
+//             return check;
 
-        }
-        return check;
-    }
+//         }
+//         return check;
+//     }
     
     
-    // set orderID for customer on page load
-    sessionStorage.setItem('orderID', createOrderID())
-})
+//     // set orderID for customer on page load
+//     sessionStorage.setItem('orderID', createOrderID())
+// })
