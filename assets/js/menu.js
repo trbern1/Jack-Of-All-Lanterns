2416cbdb06e2ce219b4d397c5714bb61f0c37663
Tegan Bernard <trbern1@ilstu.edu>
2022-04-22 16:35:13 -0500

    added basic logic for menu buttons

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,23 +1,28 @@
 $(document).ready(function() {
+    $(".menu-btn").click(function () {
+        sessionStorage.setItem(this.id,1);
+        console.log("added " + this.id + " to storage.")
+})
 
-    if(sessionStorage.hasOwnProperty("orderID")) {
-        alert("orderID already set")
-    }
 
-    function setItemID() {
-        // retrieve quantity from cart
-        var quantity = 0;
-        // set quantity in session storage
-        sessionStorage.setItem('itemID', quantity)
-    }
+    // if(sessionStorage.hasOwnProperty("orderID")) {
+    //     alert("orderID already set")
+    // }
 
-    function getMenuItems() {
-        const menuItemUrl = ''
-        $.get(menuItemUrl, function(data, status) {
-            alert("Data: " + data + "\nStatus: " + status);
-        }).fail(function() {
-            console.error("Error while getting menu items")
-        })
-        return data;
-    }
+    // function setItemID() {
+    //     // retrieve quantity from cart
+    //     var quantity = 0;
+    //     // set quantity in session storage
+    //     sessionStorage.setItem('itemID', quantity)
+    // }
+
+    // function getMenuItems() {
+    //     const menuItemUrl = ''
+    //     $.get(menuItemUrl, function(data, status) {
+    //         alert("Data: " + data + "\nStatus: " + status);
+    //     }).fail(function() {
+    //         console.error("Error while getting menu items")
+    //     })
+    //     return data;
+    // }
 });
