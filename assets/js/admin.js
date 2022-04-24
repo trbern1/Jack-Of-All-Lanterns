ecb01bdbf47af3752070da65b08d67c30f7fc6f3
Tegan Bernard <trbern1@ilstu.edu>
2022-04-24 09:02:42 -0500

    Input fields now show on selection - Admin Page

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -2,19 +2,31 @@ $(document).ready(function() {
     // document.getElementById
 })
 function showDiv(id, elementValue) {
-    console.log(elementValue.value)
+    // console.log(elementValue.value)
     const buttons = document.querySelectorAll(".btn-menu-crud");
         buttons.forEach(e => {
             e.style.display = "none";
     })
-    if (elementValue.value == "menu-get-all"){
-        const elements = document.querySelectorAll(".show-1");
+    if (elementValue.value == "menu-opt-1"){
+        const elements = document.querySelectorAll(".show-menu-1");
         elements.forEach(e => {
             e.style.display = "block";
         })
     }
-    else if (elementValue.value == "menu-create"){
-        const elements = document.querySelectorAll(".o2-show");
+    else if (elementValue.value == "menu-opt-2"){
+        const elements = document.querySelectorAll(".show-menu-2");
+        elements.forEach(e => {
+            e.style.display = "block";
+        })
+    }
+    else if (elementValue.value == "menu-opt-3"){
+        const elements = document.querySelectorAll(".show-menu-3");
+        elements.forEach(e => {
+            e.style.display = "block";
+        })
+    }
+    else if (elementValue.value == "menu-opt-4"){
+        const elements = document.querySelectorAll(".show-menu-4");
         elements.forEach(e => {
             e.style.display = "block";
         })
